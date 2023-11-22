2edec3e26f1e88b19ba436691ac3a7e0b550070c
Nico Diaz <nicodiazan@gmail.com>
2023-11-22 07:46:40 +0100

    Implement basic interaction between server and client

--- src/server/main.js
+++ src/server/main.js
@@ -3,10 +3,33 @@ const ViteExpress = require("vite-express");
 
 const app = express();
 
+const cors = require("cors");
+
 app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
+	res.send("Hello Vite + React!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
+ViteExpress.listen(app, 9000, () =>
+	console.log("Server is listening on port 9000...")
 );
+
+// serve your css as static
+app.use(express.static(__dirname + "/styles/"));
+
+app.use(cors());
+
+// app.get("/", function (req, res, next) {
+// 	res.send("API is working properly");
+// });
+
+app.get("/:page?", (req, res) => {
+	console.log(req.params.page);
+	res.send("API is working properly");
+	// res.sendFile(
+	// 	!req.params.page
+	// 		? __dirname + "/pages/index.html"
+	// 		: fs.existsSync(__dirname + "/pages/" + req.params.page + ".html")
+	// 		? __dirname + "/pages/" + req.params.page + ".html"
+	// 		: __dirname + "/pages/404.html"
+	// );
+});
